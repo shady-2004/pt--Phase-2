4c9c37ed2346419897be21c8dcb5d47d2fa5d8d6
blitzaa <saleh.ahmed04@eng-st.cu.edu.eg>
2023-12-26 13:06:23 +0200

    حنكشة كمان مرة اني اسف

--- Actions/AddFigureAction.cpp
+++ Actions/AddFigureAction.cpp
@@ -3,14 +3,19 @@
 
 AddFigureAction::AddFigureAction(ApplicationManager* pApp) : Action(pApp)
 {
+	F = NULL;
 }
 
 void AddFigureAction::UndoExecution()
 {
+	if (F == NULL)
+		return;
 	pManager->DeleteFigure(F);
 }
 
 void AddFigureAction::RedoExecution()
 {
+	if (F == NULL)
+		return;
 	pManager->AddFigure(F);
 }
